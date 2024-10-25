81a679995e9ca7820ad6203614e2a0082542803b
张艺严 <zhangyiyan@zhangyiyandembp.mynetworksettings.com>
2024-10-25 02:10:17 -0400

    add log

--- app/main.py
+++ app/main.py
@@ -4,10 +4,12 @@ from app import models
 from app.database import engine, get_db
 from app.models import review # 导入所有模型
 from app.routers.review_service import review_router  # 导入评论相关的 router
-
+from app.config.log import setup_logger
+from app.dependecies.logging_middleware import logging_dependency
 # 创建数据库表
 review.Base.metadata.create_all(bind=engine)
 
+logger = setup_logger()
 
 
 app = FastAPI()
@@ -15,6 +17,7 @@ app = FastAPI()
 
 # 这是一个router的示例
 app.include_router(review_router, prefix="/reviews", tags=["reviews"])
+app.middleware("http")(logging_dependency)
 
 @app.get("/")
 def read_root():
